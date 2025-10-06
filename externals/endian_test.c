de508eab195994474ebfabc5d484058ec07d5f56
haruo <takeuchigabriel@usp.br>
2025-10-06 17:21:46 -0300

    Add phony endian test file and update vorpal module and register types

---
+++ externals/endian_test.c
@@ -0,0 +1,6 @@
+/* 
+ * Phony endian test file - required by CMakeLists.txt configure_file
+ * The actual endianness detection is handled by CMake's TestBigEndian module
+ */
+
+/* This file exists only to satisfy the configure_file requirement */
